e9f3caba0f93af970a8cca5778150b2bada286b3
AttreaVe <johansiautama@gmail.com>
2017-07-18 10:51:00 +0700

    revert learning pake style dosen
    error mas bbro

--- resources/views/template/meta.blade.php
+++ resources/views/template/meta.blade.php
@@ -1,7 +1,6 @@
 	<meta charset="utf-8">
 
 	<link rel="stylesheet" href="{{ URL::asset('css/core.style.css') }}" type="text/css">
-	<link href="{{ URL::asset('css/stylesDosen.css') }}" rel="stylesheet" type="text/css">
 	<script src="{{ URL::asset('js/jquery.min.js') }}"></script>
 	<script src="{{ URL::asset('js/jquery.transit.min.js') }}"></script>
 	<script src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
